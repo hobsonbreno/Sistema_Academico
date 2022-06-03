722846c475914afde29fba0b88448b76a3b362b2
Hobson Nascimento <hobsonbmn@gmail.com>
2022-06-03 16:39:09 -0300

    atualização de codigos em pessoa

--- pessoa.py
+++ pessoa.py
@@ -5,6 +5,6 @@ class Pessoa:
         self.status = status 
         self.matricula = matricula
     
-    def cursando (self, curso , local, professor,matéria):
-        print(f'{self.nome} está cursando {curso} na {local} e está na diciplina de {professor} com o professor {matéria}.')
+    def cursando (self,Nacionalidade,Estado_Civil,Profissão,CPF,Endereço,bairro,cep, curso , local,cadeira , professor):
+        print(f'{self.nome},{Nacionalidade},{Estado_Civil},{Profissão},inscrito no CPF sob o nº:{CPF},\nResidente e domiciliado na:{Endereço},no bairro:{bairro},CEP:{cep},\nEstá cursando {curso} na {local},\nAtualmente está concluindo a cadeira de {cadeira} com o professor {professor}.')
         self.status = True
