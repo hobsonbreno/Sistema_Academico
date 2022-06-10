b862e2931b2a19d5bb94442203d53a3effa0eda5
Hobson Nascimento <hobsonbmn@gmail.com>
2022-06-09 21:13:26 -0300

    Refatorando o codigo

--- Funcionario.py
+++
@@ -1,7 +0,0 @@
-class Funcionario:
-    def __init__(self, nome, idade, status, matricula, cpf):
-        self.nome = nome
-        self.idade = idade
-        self.status = status 
-        self.matricula = matricula
-        self.cpf = cpf
--- __CComponentes__/testM.py
+++
@@ -1,5 +0,0 @@
-from Matricula import Matricula
-
-m1 = Matricula('isaac',25,23915565425)
-m1.Imprime()
-
