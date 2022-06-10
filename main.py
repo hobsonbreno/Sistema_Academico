c0e7040e53aa32aeb8b7be0aa5f45623aef8e977
Hobson Nascimento <hobsonbmn@gmail.com>
2022-06-10 17:16:58 -0300

    adicionando Mais pessoas

--- main.py
+++ main.py
@@ -2,3 +2,12 @@ from Pessoa import Pessoa
 
 p1 = Pessoa('Hobson Breno Moreira do Nascimento', 35)
 p1.cursando('Analise e Desenvolvimento de Sistemas')
+
+p2 = Pessoa('Wesiley', 24)
+p2.cursando('Analise e Desenvolvimento de Sistemas')
+
+p3 = Pessoa('Rayssa', 20)
+p3.cursando('Analise e Desenvolvimento de Sistemas')
+
+p4 = Pessoa('Fabricio', 24)
+p4.cursando('Analise e Desenvolvimento de Sistemas')
