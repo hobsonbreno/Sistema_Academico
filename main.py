7f2877ca07ae90dde0d04d3c5ffce5619a62cfce
Hobson Nascimento <hobsonbmn@gmail.com>
2022-06-10 16:12:26 -0300

    atualizando classes em pessoas

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from Pessoa import Pessoa 
 
 p1 = Pessoa('Hobson Breno Moreira do Nascimento', 35)
-p1.cursando('Brasileiro Nato','Casado','Empresário','013.***.***-14','Rua **, nº ** Casa altos','Messejana','60.***-***','Analise e Desenvolvimento de Sistemas','Uniateneu','POO','Sandro Mesquita')
+p1.cursando('Analise e Desenvolvimento de Sistemas')
