938f9f2ecee3f70093c7663b09cf5da10241d994
Hobson Nascimento <hobsonbmn@gmail.com>
2022-06-16 21:58:24 -0300

    Atualização de código geral V1.0

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-from Pessoa import Pessoa 
+from faculdade import Faculdade
+faculdade = Faculdade()
+faculdade.inserirAluno("xaropinho")
+faculdade.inserirMatricula("001")
+faculdade.inserirCursos("Medicina veterinaria")
+faculdade.inserirModalidade("Graduação")
 
-p1 = Pessoa('Hobson Breno Moreira do Nascimento', 35)
-p1.cursando('Analise e Desenvolvimento de Sistemas')
 
-p2 = Pessoa('Wesiley', 25)
-p2.cursando('Analise e Desenvolvimento de Sistemas')
-
-p3 = Pessoa('Rayssa', 20)
-p3.cursando('Analise e Desenvolvimento de Sistemas')
-
-p4 = Pessoa('Fabricio', 25)
-p4.cursando('Analise e Desenvolvimento de Sistemas')
+faculdade.imprimir()
