64e161b4954fed79c084042fb577617bb9a3d7da
Hobson Nascimento <hobsonbmn@gmail.com>
2022-06-21 13:41:11 -0300

    renovando codigo novamente devido aos conflitos internos

--- faculdade.py
+++ faculdade.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 
 class Faculdade():
@@ -69,32 +68,3 @@ class Funcionario(Pessoa,Faculdade):
 
 
     
-=======
-# from readline import append_history_file
-
-class Faculdade:
-    def __init__(self):
-        self.aluno = list()
-        self.matricula = list()
-        self.curso = list()
-        self.modalidade = list()
-        self.estagio = list()
-
-    def inserirAluno(self, aluno):
-        self.aluno.append(aluno)
-
-    def inserirMatricula(self, matricula):
-        self.matricula.append(matricula)
-
-    def inserirCursos(self, curso):
-        self.curso.append(curso)
-
-    def inserirModalidade(self, modalidade):
-        self.modalidade.append(modalidade)
-
-    def imprimir(self):
-        print(f'Aluno {self.aluno} da matricula {self.matricula} do curso {self.curso} esta na modalidade {self.modalidade} status Cursando ')
-
-    def InserirEstagio(self,estagio):
-        self.estagio.append(estagio)    
->>>>>>> refs/remotes/origin/main
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from faculdade import Faculdade, Aluno
 
 x = Faculdade()
@@ -24,53 +23,3 @@ x.inserirCargos("desempregada")
 
 x.imprimir()
 Aluno.imprimirAluno()
-=======
-from faculdade import Faculdade
-
-while True:
-
-    # Menu Inicial
-    print("-"*30)
-    print("""Sistema Acadêmico:
-    [ 1 ] Cadastrar Aluno
-    [ 2 ] Listar Alunos
-    [ 3 ] Sair""")
-    opcao = int(input("Digita sua opção: "))
-    print("-"*30)
-
-
-    # Opção 1: Cadastrar Aluno
-    if opcao == 1:
-        faculdade = Faculdade()
-
-        # Adicionando o Nome do Aluno
-        nome = str(input("Digite o Nome do Aluno: "))
-        faculdade.inserirAluno(nome)
-
-        # Adicionando a Matrícula do Aluno
-        matricula = int(input("Digite a Matrícula do Aluno: "))
-        faculdade.inserirMatricula(matricula)
-
-        # Adicionando o Nome do Curso do Aluno
-        curso = str(input("Digite o Nome do Curso: "))
-        faculdade.inserirCursos(curso)
-
-        # Adicionando a Modalidade do Curso
-        modalidade = str(input("Digite a Modalidade do Curso: "))
-        faculdade.inserirModalidade(modalidade)
-
-        # Testando...
-        print(len(faculdade.matricula))
-    
-
-    # Opção 2: Listar Alunos
-    elif opcao == 2:
-        print("Listar Alunos em Desenvolvimento...")
-    
-
-    # Opção 3: Sair
-    elif opcao == 3:
-        print("Saindo do Sistema...")
-        break
-
->>>>>>> refs/remotes/origin/main
