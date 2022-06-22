b6b9255352fd5ebb098b914199d09150b41d84f4
thiagoeric00 <83562406+thiagoeric00@users.noreply.github.com>
2022-06-22 19:34:23 -0300

    Reestruturação do Sistema

--- faculdade.py
+++ faculdade.py
@@ -1,71 +1,122 @@
+# Importação para usar na Classe Pai
+from abc import ABC
 
 
-class Faculdade():
+# Listas para a Classe Aluno
+nome_aluno = list()
+idade_aluno = list()
+cpf_aluno = list()
+mat_aluno = list()
+curso_aluno = list()
+
+#Listas para a Classe Funcionario
+nome_func = list()
+idade_func = list()
+cpf_func = list()
+cargo_func = list()
+
+#Lista para a Classe Curso
+nome_curso = list()
+sem_curso = list()
+mod_curso = list()
+
+
+# Classe Pai
+class Pessoa(ABC):
+
+    # Método Construtor
+    def __init__(self, nome, idade, cpf):
+        self.__nome = nome
+        self.__idade = idade
+        self.__cpf = cpf
+
+
+# Classe Filha
+class Aluno(Pessoa):
+
+    # Método Construtor
+    def __init__(self, nome, idade, cpf, matricula, curso):
+        super().__init__(nome, idade, cpf)
+        self.__matricula = matricula
+        self.__curso = curso
+
+    # Método para Cadastrar propriedades em suas listar
+    def cadastrar_aluno(self):
+
+        nome = self._Pessoa__nome
+        nome_aluno.append(nome)
+
+        idade = self._Pessoa__idade
+        idade_aluno.append(idade)
+
+        cpf = self._Pessoa__cpf
+        cpf_aluno.append(cpf)
+
+        matricula = self.__matricula
+        mat_aluno.append(matricula)
+
+        curso = self.__curso
+        curso_aluno.append(curso)
     
-    def inserirAluno(self, aluno):
-        estudante = aluno
-        alunos.append(estudante)
-
-    def inserirMatricula(self, matricula):
-        estudante = matricula
-        matriculas.append(estudante)
-
-    def inserirCurso(self, curso):
-        estudante = curso
-        cursos.append(estudante)
-
-    def inserirModalidade(self, modalidade):
-        estudante = modalidade
-        modalidades.append(estudante)
-
-    def inserirCargos(self, cargo):
-        estudante = cargo
-        cargos.append(estudante)
-
-    def imprimir(self):
-        for i in range(len(alunos)):
+    # Método para Listar
+    def listar_alunos(self):
+        for i in range(len(nome_aluno)):
             print('-'*30)
-            print(f"Alunos: {alunos[i]}\n matriculas: {matriculas[i]}\n cursos: {cursos[i]}\n modalidades: {modalidades[i]}\n cargos: {cargos[i]}")
+            print(f" Alunos: {nome_aluno[i]}\n Idade: {idade_aluno[i]}\n CPF: {cpf_aluno[i]}\n Matrícula: {mat_aluno[i]}\n Curso: {curso_aluno[i]}")
 
 
-alunos = []
-matriculas = []
-cursos = []
-modalidades = []
-cargos = []
+# Classe Filha
+class Funcionario(Pessoa):
 
-
-class Pessoa(Faculdade):
-    def __init__(self,nome,data_nascimento,cpf):
-       self.nome = nome 
-       self.data_nascimento = data_nascimento
-       self.cpf = cpf 
-
-
-class Aluno(Pessoa,Faculdade):
-    def __init__(self,matricula,curso,modalidade):
-       self.matricula = matricula
-       self.curso = curso
-       self.modalidade = modalidade
-
-
-    def logarAluno(self, aluno):   
-
-        if aluno not in self.alunos:
-            print(f'Nome ou matricula incorretos ! {self.alunos}')
-        else:
-            print(f'Acesso liberado !')
-
-    def imprimirAluno():
-        print('lista de aluno',alunos)
-
-
-class Funcionario(Pessoa,Faculdade):
-     def __init__(self,cargo):
-        self.cargos = cargo 
-
-
-
-
-
+    # Método Construtor
+    def __init__(self, nome, idade, cpf, cargo):
+        super().__init__(nome, idade, cpf)
+        self.__cargo = cargo
     
+    # Método para Cadastrar propriedades em suas listar
+    def cadastrar_func(self):
+
+        nome = self._Pessoa__nome
+        nome_func.append(nome)
+
+        idade = self._Pessoa__idade
+        idade_func.append(idade)
+
+        cpf = self._Pessoa__cpf
+        cpf_func.append(cpf)
+
+        cargo = self.__cargo
+        cargo_func.append(cargo)
+    
+    # Método para Listar
+    def listar_funcionarios(self):
+        for i in range(len(nome_func)):
+            print('-'*30)
+            print(f" Alunos: {nome_func[i]}\n Idade: {idade_func[i]}\n CPF: {cpf_func[i]}\n Cargo: {cargo_func[i]}")
+
+
+class Curso():
+
+    # Método Construtor
+    def __init__(self, nome, semestres, modalidade):
+        self.__nome = nome
+        self.__semestres = semestres
+        self.__modalidade = modalidade
+    
+    # Método para Cadastrar propriedades em suas listar
+    def cadastrar_curso(self):
+
+        nome = self.__nome
+        nome_curso.append(nome)
+
+        semestre = self.__semestres
+        sem_curso.append(semestre)
+
+        modalidade = self.__modalidade
+        mod_curso.append(modalidade)
+
+    # Método para Listar
+    def listar_cursos(self):
+        for i in range(len(nome_curso)):
+            print('-'*30)
+            print(f" Nome: {nome_curso[i]}\n Semestres: {sem_curso[i]}\n CPF: {mod_curso[i]}")
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from faculdade import Faculdade
+from faculdade import Aluno, Funcionario, Curso
 
 while True:
 
@@ -6,36 +6,74 @@ while True:
     print("="*30)
     print("""Sistema Acadêmico:
     [ 1 ] Cadastrar Aluno
-    [ 2 ] Listar Alunos
-    [ 3 ] Sair""")
+    [ 2 ] Cadastrar Funcionario
+    [ 3 ] Cadastrar Curso
+    [ 4 ] Listar Alunos
+    [ 5 ] Listar Funcionario
+    [ 6 ] Listar Curso
+    [ 7 ] Sair""")
     opcao = int(input("Digita sua opção: "))
     print("="*30)
 
-    # Criação do Objeto
-    x = Faculdade()
-
     # Opção 1: Cadastrar Dados do Aluno
     if opcao == 1:
-        aluno = str(input("Digite o nome do aluno: "))
-        x.inserirAluno(aluno)
 
-        matricula = int(input("Digite o nome do matricula: "))
-        x.inserirMatricula(matricula)
+        # Inputs para inserir os dados
+        nome_aluno = str(input("Digite o Nome: "))
+        idade_aluno = int(input("Digite a Idade: "))
+        cpf_aluno = int(input("Digite o CPF: "))
+        matricula_aluno = int(input("Digite a Matrícula: "))
+        curso_aluno = str(input("Digite o Curso: "))
 
-        curso = str(input("Digite o nome do curso: "))
-        x.inserirCurso(curso)
+        # Cadastrando o Aluno, criando um objeto e usando o método 
+        aluno = Aluno(nome_aluno, idade_aluno, cpf_aluno, matricula_aluno, curso_aluno)
+        aluno.cadastrar_aluno()
+    
 
-        modalidade = str(input("Digite o nome do modalidade: "))
-        x.inserirModalidade(modalidade)
+    # Opção 2: Cadastrar Dados do Funcionarios
+    if opcao == 2:
+        
+        # Inputs para inserir os dados
+        nome_func = str(input("Digite o Nome: "))
+        idade_func = int(input("Digite a Idade: "))
+        cpf_func = int(input("Digite o CPF: "))
+        cargo_func = str(input("Digite o Cargo: "))
 
-        cargos = str(input("Digite o nome do cargos: "))
-        x.inserirCargos(cargos)
+        # Cadastrando o Funcionário, criando um objeto e usando o método
+        func = Funcionario(nome_func, idade_func, cpf_func, cargo_func)
+        func.cadastrar_func()
 
-    # Opção 2: Listar Dados dos Alunos
-    elif opcao == 2:
-        x.imprimir()
 
-    # Opção 3: Sair do Sistema
-    elif opcao == 3:
+    # Opção 3: Cadastrar Dados do Cursos
+    if opcao == 3:
+
+        # Inputs para inserir os dados
+        nome_curso = str(input("Digite o Nome: "))
+        semestres_curso = int(input("Digite a quantidade de Semestres: "))
+        modalidade_curso = str(input("Digite a Modalide do Curso: "))
+
+        # Cadastrando o Curso, criando um objeto e usando o método
+        curso = Curso(nome_curso, semestres_curso,modalidade_curso)
+        curso.cadastrar_curso()
+
+
+    # Opção 4: Listar Dados dos Alunos
+    elif opcao == 4:
+        aluno.listar_alunos()
+
+
+    # Opção 5: Listar dados dos Funcionarios
+    elif opcao == 5:
+        func.listar_funcionarios()
+
+
+    # Opção 6: Listar dados dos Cursos
+    elif opcao == 6:
+        curso.listar_cursos()
+
+
+    # Opção 7: Sair do Sistema
+    elif opcao == 7:
         print("Sistema encerrado!")
         break
+
